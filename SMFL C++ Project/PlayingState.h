967ea7edb5571efb06c94f5c805697acd9f3a657
Sofian <moumensofian@gmail.com>
2020-02-03 07:06:07 +0100

    modified some irrelevant stuff

---
+++ SMFL C++ Project/PlayingState.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "State.h"
+#include "Bullet.h"
+class Player;
+
+
+class PlayingState :
+	public State
+{
+public :
+	PlayingState(StateMachine& machine, sf::RenderWindow& window, bool replace = true);
+
+	~PlayingState();
+
+	void UpdateKeyboardInputs(sf::Keyboard::Key key, bool isPressed);
+
+	void UpdateEvents();
+	void Update();
+	void Render();
+
+private:
+	//Variables and Booleans
+
+
+
+	//Arrays
+
+
+
+	//Vectors
+
+
+
+	//Class objects
+	std::unique_ptr<Player> player = nullptr;
+	std::unique_ptr<Bullet> pBullet = nullptr;
+	bool GamePaused;
+//	FPSCounter fpsCounter;
+//	Text versionText;
+	bool playerShooting;
+
+	//SFML
+	sf::Event sfEvent;
+	sf::Texture playerTexture;
+	sf::Texture bulletTexture;
+	sf::Clock dtClock;
+	float	  dtTimer;
+};
+
+
+
+
