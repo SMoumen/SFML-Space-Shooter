720b0a15d1aeec5c1c5c1539bde6373ba83d6174
Sofian <moumensofian@gmail.com>
2020-03-06 09:16:13 +0100

    added macro for title font

--- SMFL C++ Project/Define.h
+++ SMFL C++ Project/Define.h
@@ -2,3 +2,6 @@
 constexpr float screen_width = 1280.0f;
 constexpr float screen_height = 900.0f;
 constexpr float framerate_limit = 60.0f;
+
+#define FONT "Arial.ttf"
+#define FONT_TITLE "title_font.ttf"
